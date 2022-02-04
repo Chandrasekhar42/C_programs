53701814a49487e943a78d8b34420ca6c853f643
chandu <chandrasekhar.p@thundersoft.com>
2022-02-04 09:24:26 +0530

    feb2 1st program changed

--- feb_2/4_program.c
+++ feb_2/4_program.c
@@ -1,31 +1,34 @@
 #include<stdio.h>
-#include<stdlib.h>
+
 int main()
-  {
-    int size,*ptr;
-
-    printf("enter the range of sequence:");
-
-    scanf("%d\n",&size);
-
-    ptr = (int *)malloc(sizeof(int)*size);
-
-    int temp = *ptr;
-   
-    int tmpsize=size; 
-   
-     while(size>0)
-             {
-                 scanf("%d",ptr);
-                 ptr++;
-                 size--;
-             }
-      ptr = temp;
-     while(tmpsize>0)
-                {
-                   printf("%d",(int *)*ptr);
-                     ptr++;
-                   tmpsize--;
-                }
-      
-  }
+{
+    int arr[]={1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
+    int a,i,count,val=0,size,add=-1;
+    size = sizeof(arr)/sizeof(arr[0]);
+  
+    while(val<size)
+    {
+       i=val;
+       count=0;
+       a=0;
+    while(arr[i]==arr[val])
+    {
+        i++;
+        count++;
+    }
+    if(count==arr[val])
+    {
+        ++add;
+        val += add;      
+        a=1;
+    }
+        else
+          break;
+    val++;
+    }
+    if(a)
+        printf("valid\n");
+        else
+    printf("Invalid\n");
+    return 0;
+}
